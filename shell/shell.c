c8f30f2d32e3c121d86cd5f0da8ff6953ea273ba
Ulises <ulimartinez96@gmail.com>
2017-10-01 01:37:57 -0600

    need to fix loop

--- shell/shell.c
+++ shell/shell.c
@@ -45,7 +45,8 @@ int main(int argc, char **argv, char **envp){
         if(streq(str, "exit"))//check for exit string
             return 0;
         char **pipes = mytoc(str, PIPE_DELIM);//separate the input by commands that use pipes
-        for(int i = 0; i < veclen(pipes); i++){
+        int length = veclen(pipes);
+        for(int i = 0; i < length; i++){
             char **tokens = mytoc(pipes[i], SPACE_DELIM);//tokenize the input from the prompt
             if(tokens){
                 if(streq(tokens[0], "cd")){//if the first token is cd then
@@ -60,7 +61,7 @@ int main(int argc, char **argv, char **envp){
                 else{//if the command was found create a child process to run the command
                     pid = fork();
                     if(pid == 0){//if child
-                        if(veclen(pipes) > 1){//case where we will need to pipe the output of the child
+                        if(length > 1 && i+1 != length){//case where we will need to pipe the output of the child
                             close(pipedes[PIPE_READ_END]);//close the read end of the pipe (wont read anything from it)
                             dup2(pipedes[PIPE_WRITE_END], STDOUT_FILENO);//dup the write end of the pipe to stdout, dup2 closes the fd if it is open
                             close(pipedes[PIPE_WRITE_END]);//close the write end of the pipe TODO: don't close it if you will write to it again
@@ -74,31 +75,14 @@ int main(int argc, char **argv, char **envp){
                                 printf("Program terminated with exit code %d\n", waitStatus);
                             }
                         }
-                        if(veclen(pipes) > 1){//case where we are using a pipe
+                        if(length > 1 && length == i + 1){//reset the fds if the last command returned
+                            close(pipedes[PIPE_READ_END]);//close the read end of pipe
+                            dup2(cstdin, STDIN_FILENO);//reset the fd 0
+                            dup2(cstdout, STDOUT_FILENO);//reset fd 1
+                        }
+                        else if(length > 1){//case where there are more commands that will read from pipe
                             close(pipedes[PIPE_WRITE_END]);//close the write end of the pipe
                             dup2(pipedes[PIPE_READ_END], STDIN_FILENO);//dup the read end of the pipe to stdin
-                            pid = fork();//create a new child process that will read input from the pipe
-                            if(pid == 0){//second child
-                                char **tokens2 = mytoc(pipes[1], SPACE_DELIM);//tokenize what's after the pipe
-                                char *command2 = getAbsComm(tokens2[0], paths, envp);
-                                if(command2)
-                                    execve(command2, tokens2, envp);
-                                else{
-                                    printf("%s: command not found\n", tokens2[0]);
-                                }
-                                //cleanup
-                                free(command2);
-                                freeVec(tokens2);
-                            }
-                            else{
-                                waitVal = (pid, &waitStatus, 0);//wait until child terminates
-                                if(waitVal == pid){//if the terminated child notify the exit code
-                                    printf("Program terminated with exit code %d\n", waitStatus);
-                                }
-                                close(pipedes[PIPE_READ_END]);//close the read end of pipe
-                                dup2(cstdin, STDIN_FILENO);//reset the fd 0
-                                dup2(cstdout, STDOUT_FILENO);//reset fd 1
-                            }
                         }
                     }
                 }
